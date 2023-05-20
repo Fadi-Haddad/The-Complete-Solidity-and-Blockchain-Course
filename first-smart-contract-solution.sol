341bf15de6dafb555a77bac3bd846032db5854ce
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-21 00:33:16 +0300

    first commit

---
+++ first-smart-contract-solution.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract WelcomeToSolidity {
+   constructor() public{
+   }
+   
+   // is adding two integers
+   function getResult() public view returns(uint){
+      uint a = 1;
+      uint b = 14;
+      uint result = a + b;
+      return result;
+   }
+}
