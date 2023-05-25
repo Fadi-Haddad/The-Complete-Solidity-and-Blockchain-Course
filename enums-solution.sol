f79bc6b35a6bf93558c09a3fe074fc2a2d9698f5
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-25 11:46:00 +0300

    Add enums-solution.sol

---
+++ enums-solution.sol
@@ -0,0 +1,24 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+// 1. create an enum for the color of shirts called shirtColor and set it to the options of either RED or WHITE or BLUE
+// 2 create a data of shirtColor called defaultChoice which is a constant set to the color BLUE 
+// 3. create a data of shirtColor called choice and don't initiate the value
+// 4. create a function called setWhite which changes the shirt color of shirtColor to white
+// 5. create a function getChoice which returns the current choice of shirtColor
+// 6. create a function getDefaultChoice which returns the default choice of shirtColor
+
+    enum shirtColor {RED, WHITE, BLUE}
+    shirtColor choice;
+    shirtColor constant defaultChoice = shirtColor.BLUE;
+    
+    function setWhite() public {
+        choice = shirtColor.WHITE;
+    }
+    
+    function getChoice() public view returns(shirtColor) {
+        return choice;
+    }
+    
+        function getDefaultChoice() public view returns(uint) {
+        return uint(defaultChoice);
+    }
