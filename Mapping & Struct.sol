c0838a3389d4701b8cd42e25a6aae8fd0262e7d5
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-29 20:58:37 +0300

    Add Mapping&struct.sol

---
+++ Mapping & Struct.sol
@@ -0,0 +1,22 @@
+// Mapping Assignment:
+
+// 0. Create a unique data type as a struct called movie and give it the string properties: title and director. 
+// 1. Create a map called movie wich takes a uint as a key and Movie as a value 
+// 2, create a function called addMovie which takes three inputs, movie id, title and director which 
+//    assigns a value of an integer to a movie added back to the movie map. It should include a title and director name,
+// 3. Deploy the contract and update the movie interaction to the movie mop with our Favorite movies.
+
+pragma solidity = 0.5.0;
+
+contract MovieContract {
+
+mapping (uint => Movie) movie;
+struct Movie {
+    string title;
+    string director;
+}
+function addMovie(uint id, string memory title, string memory director) public {
+    movie[id] = Movie (title, director);
+}
+
+}
