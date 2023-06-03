9e0cda4d62563cd6de43ee4eeea0dd82a64f2a83
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-06-03 17:28:20 +0300

    Add a minting and a sending function

--- my-first-ERC20-Token.sol
+++ my-first-ERC20-Token.sol
@@ -1,11 +1,29 @@
 pragma solidity >=0.5.0 <0.7.0;
 
 contract Coin {
+
     address public minter;
     mapping (address => uint) public balances;
-    
+    error insufficientBalance(uint requested, uint available);
+
     constructor () {
-        minter =msg.sender;
+        minter = msg.sender;
     }
 
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount){
+            revert insufficientBalance({
+                requested: amount,
+                available:balances[msg.sender]
+            });
+        }
+        
+        balances[msg.sender]-= amount;
+        receiver += amount;
+    }
 }
