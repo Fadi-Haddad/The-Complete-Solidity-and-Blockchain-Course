8b78681e2c605df92fb2f56feec40d18ad7c3937
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-06-05 00:49:11 +0300

    add a disown function witht a time condition

--- restricred-access.sol
+++ restricred-access.sol
@@ -12,12 +12,23 @@ contract RestrictedAcces {
     // solution:
     modifier onlyBy(address _account) {
         require(msg.sender == _account,
-        'Sender not authorized!'
-        );
+        'Sender not authorized!');
         _;
     } 
 
-    
+    // write a function that can disown the current owner 
+    // only run the function 10 seconds after the creation of the contract 
+
+    // solution:
+    modifier onlyAfter(uint _time) {
+        require(block.timestamp >= _time,
+        'function is called too early!');
+        _;
+    }
+    function disown() onlyBy(owner) onlyAfter(creationTime + 10 seconds) public {
+        delete owner;
+    } 
+
     function changeOwnerAddress(address _newAddress) onlyBy(owner) public {
         owner = _newAddress;
     }
