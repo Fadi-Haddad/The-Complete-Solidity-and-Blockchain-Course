e88dfcfbc25093bb8920411f9e2e00855a9a01e4
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-06-05 14:04:53 +0300

    Add cost modifier

--- restricred-access.sol
+++ restricred-access.sol
@@ -25,6 +25,7 @@ contract RestrictedAcces {
         'function is called too early!');
         _;
     }
+
     function disown() onlyBy(owner) onlyAfter(creationTime + 10 seconds) public {
         delete owner;
     } 
@@ -32,5 +33,28 @@ contract RestrictedAcces {
     function changeOwnerAddress(address _newAddress) onlyBy(owner) public {
         owner = _newAddress;
     }
+
+    /*
+    Exercise: 
+    1. Create a modifier called costs which takes an _amount parameter
+    2. Require that msg.value is greater than or equal to the amount 
+    3. If the msg.value is not greather than or equal to the amount 
+       return a string that says Not enough Ether provided. 
+    4. Write a funciton called forceOwnerChange which takes an address called _newOwner and is payable 
+    5. add a modification to the signature so that the function needs 200 ether to execute 
+    6. set the owner of the contract to the new owner of the address.
+    */
+
+    // Solution:
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount,
+        'Not enough Ether provided');
+        _;
+    }
     
+    function forceOwnerChange(address _newOwner) payable costs(200 ether) public {
+        owner = _newOwner;
+    }
+
 }
