ba7d77732d6e3fb91243ed3c2f4aa9bebc3e6b73
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-06-11 21:58:07 +0300

    update error-handling

--- error-handling.sol
+++ error-handling.sol
@@ -60,3 +60,32 @@ contract LearnErrorHandling {
         }
     }
 }
+
+/*
+Exercise:
+1. Create a contract called Vendor with the state variable address seller. 
+It should contain a modifier onlySeller that requires the msg.sender to be the seller.
+2. Add a function becomeSeller which sets the seller to the msg.sender.
+3. Create a function named sell which is payable and checks so see 
+if an input of an amount is greater than the msg.value and in the event
+to revert that there is not enough ether provided as a throw error. 
+*/
+
+contract Vendor {
+    address seller;
+    
+    modifier onlySeller() {
+          require(msg.sender == seller,
+          'Only the seller can sell this!');
+          _;
+    }
+    function becomeSeller() public {
+        seller = msg.sender;
+    }
+    
+    function sell(uint amount) payable public onlySeller {
+        if(amount > msg.value) {
+            revert('There is not enough ether provided!');
+        }
+    }
+}
