e7e91bc46b4cb17705bf76677ef5a64afdf371cb
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-06-17 21:00:59 +0300

    Add withdraw function

--- auction-solidity-solution.sol
+++ auction-solidity-solution.sol
@@ -70,5 +70,17 @@ contract Auction {
         highestbid = msg.value;
         emit highestBidIncreased(msg.sender, msg.value);
     }
+    
+    function withdraw() public payable returns(bool) {
+    uint amount = pendingReturns[msg.sender];
+    if(amount > 0) {
+        pendingReturns[msg.sender] = 0;
+    }
+    
+    if(!payable(msg.sender).send(amount)) {
+        pendingReturns[msg.sender] = amount;
+    }
+    return true;
+}
 
 }
