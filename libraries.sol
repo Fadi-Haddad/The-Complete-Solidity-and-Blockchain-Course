39f64b1bb60dd5abf744843ff92f7bf687586c00
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-06-21 21:57:19 +0300

    Add Test Contract

--- libraries.sol
+++ libraries.sol
@@ -22,3 +22,19 @@ library Search{
         for(uint i=0; i<self.length; i++) if(self[i] == value) return i;
     }
 }
+contract Test {
+    uint[] data;
+    constructor() public {
+        data.push(1);
+        data.push(2);
+        data.push(3);
+        data.push(4);
+        data.push(5);
+    }
+    
+    function isValuePresent(uint val) external view returns(uint) {
+       uint value = val;
+       uint index = Search.indexOf(data, value); 
+       return index;
+    }
+}
