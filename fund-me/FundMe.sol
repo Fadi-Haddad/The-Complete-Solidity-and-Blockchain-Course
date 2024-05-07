7bd04f6965d86be47a546601630bc94c0a240045
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-07 20:33:15 +0300

    Add fund and withdraw functions

---
+++ fund-me/FundMe.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.8;
+
+
+contract FundMe{
+
+    function fund() public payable {
+
+        require(msg.value > 1e18, "amount not enough");
+    }
+
+    function withdraw() public {}
+
+}
